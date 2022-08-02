04f82a53fc876202db6af103641553afe61cfe62
Sam <bills2021e@gmail.com>
2022-08-01 20:23:52 -0400

    Render nav bar dynamically with JS

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import renderNav from './nav.js';
+
+renderNav();
--- src/nav.js
+++ src/nav.js
@@ -1,3 +1,49 @@
+import Icon from './coffee.png';
+
 export default function renderNav() {
+    let content = document.querySelector('#content');
     
+    let nav = document.createElement('div');
+    nav.setAttribute('id', 'nav');
+    content.appendChild(nav);
+
+    let branding = document.createElement('div');
+    branding.classList.add('branding');
+
+    let coffee = new Image();
+    coffee.src = Icon;
+    branding.appendChild(coffee);
+
+    let logo = document.createElement('h1');
+    logo.classList.add('logo');
+    logo.textContent = 'Java Script';
+    branding.appendChild(logo);
+
+    nav.appendChild(branding);
+
+    let sections = document.createElement('div');
+    sections.classList.add('sections');
+
+    let homeBtn = document.createElement('button');
+    homeBtn.classList.add('home');
+    homeBtn.textContent = 'Home';
+    sections.appendChild(homeBtn);
+
+    let menuBtn = document.createElement('button');
+    menuBtn.classList.add('menu');
+    menuBtn.textContent = 'Menu';
+    sections.appendChild(menuBtn);
+
+    let aboutBtn = document.createElement('button');
+    aboutBtn.classList.add('about');
+    aboutBtn.textContent = 'About';
+    sections.appendChild(aboutBtn);
+
+    let coffee2 = new Image();
+    coffee2.src = Icon;
+    sections.appendChild(coffee2);
+
+    nav.appendChild(sections);
+
+    content.appendChild(nav);
 }
